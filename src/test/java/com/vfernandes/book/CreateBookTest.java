aaf0f06c3ed8d212b61a0e47b123cd21379065ec
Vitor Hugo Silva Fernandes <51928276+VitorFernandes2@users.noreply.github.com>
2024-08-16 14:00:59 +0100

    Update CreateBookTest.java

--- src/test/java/com/vfernandes/book/CreateBookTest.java
+++ src/test/java/com/vfernandes/book/CreateBookTest.java
@@ -1,16 +1,23 @@
 package com.vfernandes.book;
 
 import com.vfernandes.book.controller.dto.BookDto;
+import com.vfernandes.book.database.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class CreateBookTest extends BaseIntegrationTest {
 
     private static final String AUTHOR_NAME = "Author";
     private static final String BOOK_TITLE = "Title";
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Test
     void createBookWithSuccess() {
@@ -22,6 +29,11 @@ class CreateBookTest extends BaseIntegrationTest {
                 Void.class);
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        bookRepository.findById(1L).ifPresentOrElse(book -> assertAll(
+                () -> assertEquals(BOOK_TITLE, book.getTitle()),
+                () -> assertEquals(AUTHOR_NAME, book.getAuthor())
+        ), () -> fail("Book not found"));
     }
 
     @Test
